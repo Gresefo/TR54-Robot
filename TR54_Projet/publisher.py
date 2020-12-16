a13541c6e91cb144a6c4e60377863edd115ee61c
Gresefo <loic-martin@laposte.net>
2020-12-16 20:05:04 +0100

    comments in publisher

--- TR54_Projet/publisher.py
+++ TR54_Projet/publisher.py
@@ -5,8 +5,16 @@ import threading
 import time
 
 class publisher(threading.Thread):
+    """A thread to pusblish information using a MQQT broker"""
 
     def __init__(self,ip,id,s,delta_time):
+        """
+        Constructor
+        :param ip : the IP adresse to connect
+        :param id : the robot ID (1, 2 or 3)
+        :param s : a string that indicates with information to send in order to get the correct topic
+        :param delta_time : the time between two sending of the information
+        """
         threading.Thread.__init__(self)
         self._client = MQTTClient(id,ip)
         self._client.connect()
@@ -17,11 +25,19 @@ class publisher(threading.Thread):
         self._msg = ""
         self._delta_time = delta_time
 
+
     def setMessage(self, msg):
+        """
+        Sets the message to send
+        :param msg : String the message to send
+        """
         self._msg = msg
 
 
     def run(self):
+        """
+        The run function for the thread
+        """
         while True:
             self._client.publish(self._topic,self._msg)
             time.sleep(self._delta_time)
