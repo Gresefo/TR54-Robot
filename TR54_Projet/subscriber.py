6911e7e22c33e1dc17dfe77abe48b14a3425c0bb
Snapbarre <44733998+Snapbarre@users.noreply.github.com>
2020-12-16 10:44:09 +0100

    Update subscriber.py
    gestion de la localisation et de la reception du message pour le passer au robot

--- TR54_Projet/subscriber.py
+++ TR54_Projet/subscriber.py
@@ -12,15 +12,26 @@ class subscriber(threading.Thread):
         print(type(self._topic))
         if bytes(self._topic,'utf-8') == topic:
             brick.display.text(msg.decode('utf-8'))
+            txt = msg.decode('utf-8')
             print(msg.decode('utf-8'))
+            splt = txt.split('|')
+            if(len(splt) >1 ):
+                print(splt[1])
+            if(splt[0]=="stop"):
+                self.robot.allowed = False
+            elif(splt[0]=="go"):
+                self.robot.allowed = True
+                
 
-    def __init__(self,ip,id,topic):
+    def __init__(self,ip,id,topic,robot):
         threading.Thread.__init__(self)
         self._client = MQTTClient(id,ip)
         self._client.connect()
         self._client.set_callback(self.getmessages)
         self._client.subscribe(topic)
         self._topic = topic
+
+        self.robot = robot
 
     def run(self):
         self._client.publish(self._topic,'Listening')
